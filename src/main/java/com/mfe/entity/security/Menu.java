40203996bb4727c761c7abbf0ab2b4aa01efc8e4
zichao.wang <191802799@qq.com>
2015-05-10 12:17:37 +0800

    菜单类
    存储用户自定义菜单，对菜单进行增删改查

---
+++ src/main/java/com/mfe/entity/security/Menu.java
@@ -0,0 +1,16 @@
+package com.mfe.entity.security;
+
+public class Menu {
+	/**
+	 * 菜单唯一标识
+	 */
+	private String menuKey;
+	/**
+	 * 菜单父节点
+	 */
+	private String parentID;
+	/**
+	 * 菜单名称
+	 */
+	private String menuName;
+}
